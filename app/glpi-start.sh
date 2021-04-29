65c72b222009c8934d0e01bbccad5ae2603a1003
Abner Rizzi <abner.rizzi@gmail.com>
2021-04-29 15:00:37 -0300

    GLPI

---
+++ app/glpi-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. /opt/glpi-install.sh
+
+#Start cron service
+service cron start
+
+#Activation du module rewrite d'apache
+a2enmod rewrite && service apache2 restart && service apache2 stop
+
+#Lancement du service apache au premier plan
+/usr/sbin/apache2ctl -D FOREGROUND
